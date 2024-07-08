774761292d9cd41448eef0523ccbb14d03dce49b
pguijas <pguijas@gmail.com>
2024-07-08 23:57:37 +0200

    hot-fix - proto broken by ruff

--- p2pfl/communication/grpc/proto/generate_proto.py
+++ p2pfl/communication/grpc/proto/generate_proto.py
@@ -2,7 +2,7 @@ import grpc_tools
 from grpc_tools import protoc
 
 if __name__ == "__main__":
-    # python -m grpc_tools.protoc -I=p2pfl/proto --python_out=p2pfl/proto --grpc_python_out=p2pfl/proto --mypy_out=p2pfl/proto p2pfl/proto/node.proto
+    # python -m grpc_tools.protoc -I=p2pfl/communication/grpc/proto --python_out=p2pfl/communication/grpc/proto --grpc_python_out=p2pfl/communication/grpc/proto --mypy_out=p2pfl/communication/grpc/proto p2pfl/communication/grpc/proto/node.proto
 
     GRPC_PATH = grpc_tools.__path__[0]
     PROTO_PATH = "p2pfl/communication/grpc/proto"
