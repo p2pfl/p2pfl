4c593c1b1b8d5923cfdce9f8741b7c7638ac33c9
Angel3245 <joseangelperezgarrido@gmail.com>
2024-08-26 10:40:15 +0200

    chore: Add an interface for NodeState actors

--- p2pfl/node_state.py
+++ p2pfl/node_state.py
@@ -17,14 +17,13 @@
 #
 """Node state."""
 
-import threading
+import ray
 from typing import Dict, List, Optional
 
 from p2pfl.learning.learner import NodeLearner
-from p2pfl.management.signal import SignalActor
 
-
-class NodeState:
+@ray.remote
+class NodeStateActor:
     """
     Class to store the main state of a learning node.
 
@@ -81,6 +80,39 @@ class NodeState:
         #self.model_initialized_lock = threading.Lock()
         #self.model_initialized_lock.acquire()
 
+    def get_addr(self):
+        return self.addr
+    
+    def get_status(self):
+        return self.status
+    
+    def get_actual_exp_name(self):
+        return self.actual_exp_name
+    
+    def get_total_rounds(self):
+        return self.total_rounds
+    
+    def get_simulation(self):
+        return self.simulation
+    
+    def get_learner(self):
+        return self.learner
+    
+    def get_models_aggregated(self):
+        return self.models_aggregated
+    
+    def get_nei_status(self):
+        return self.nei_status
+    
+    def get_round(self):
+        return self.round
+    
+    def get_train_set(self):
+        return self.train_set
+    
+    def get_train_set_votes(self):
+        return self.train_set_votes
+    
     def set_experiment(self, exp_name: str, total_rounds: int) -> None:
         """
         Set the experiment name.
@@ -118,3 +150,99 @@ class NodeState:
     def __str__(self) -> str:
         """String representation of the node state."""
         return f"NodeState(addr={self.addr}, status={self.status}, actual_exp_name={self.actual_exp_name}, round={self.round}, total_rounds={self.total_rounds}, simulation={self.simulation}, learner={self.learner}, models_aggregated={self.models_aggregated}, nei_status={self.nei_status}, train_set={self.train_set}, train_set_votes={self.train_set_votes})"
+
+class NodeState():
+    def __init__(self, addr: str, simulation: bool=False) -> None:
+        self.state = ray.put(NodeStateActor.remote(
+            addr,
+            simulation=simulation
+        ))
+
+    @property 
+    def addr(self):
+        state = ray.get(self.state)
+        return state.get_addr.remote()
+
+    @property 
+    def status(self):
+        state = ray.get(self.state)
+        return state.get_status.remote()
+
+    @property 
+    def actual_exp_name(self):
+        state = ray.get(self.state)
+        return state.get_actual_exp_name.remote()
+
+    @property 
+    def total_rounds(self):
+        state = ray.get(self.state)
+        return state.get_total_rounds.remote()
+
+    @property 
+    def simulation(self):
+        state = ray.get(self.state)
+        return state.get_simulation.remote()
+
+    @property 
+    def learner(self):
+        state = ray.get(self.state)
+        return state.get_learner.remote()
+
+    @property 
+    def models_aggregated(self):
+        state = ray.get(self.state)
+        return state.get_models_aggregated.remote()
+
+    @property 
+    def nei_status(self):
+        state = ray.get(self.state)
+        return state.get_nei_status.remote()
+
+    @property
+    def round(self):
+        state = ray.get(self.state)
+        print(ray.get(state.get_round.remote()))
+        return state.get_round.remote()
+    
+    @property 
+    def train_set(self):
+        state = ray.get(self.state)
+        return state.get_train_set.remote()
+
+    @property 
+    def train_set_votes(self):
+        state = ray.get(self.state)
+        return state.get_train_set_votes.remote()
+
+    def set_experiment(self, exp_name: str, total_rounds: int) -> None:
+        """
+        Set the experiment name.
+
+        Args:
+            exp_name: The name of the experiment.
+            total_rounds: The total rounds of the experiment.
+
+        """
+        state = ray.get(self.state)
+        return state.increase_round.remote(exp_name, total_rounds)
+
+    def increase_round(self) -> None:
+        """
+        Increase the round number.
+
+        Raises:
+            ValueError: If the round is not initialized.
+
+        """
+        state = ray.get(self.state)
+        return state.increase_round.remote()
+
+    def clear(self) -> None:
+        """Clear the state."""
+        state = ray.get(self.state)
+        return state.clear.remote()
+
+    def __str__(self) -> str:
+        """String representation of the node state."""
+        state = ray.get(self.state)
+        return state.__str__.remote()
